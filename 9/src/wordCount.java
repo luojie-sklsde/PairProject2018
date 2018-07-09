a159a0db2de550ffdb7d49fcb2b19450626f8087
linlkg <527564460@qq.com>
2018-07-09 20:28:32 +0800

    l

--- 9/src/wordCount.java
+++ 9/src/wordCount.java
@@ -1,9 +1,8 @@
 import jdk.nashorn.internal.runtime.regexp.joni.Regex;
-
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class wordCount {
     public static void main(String[] args) throws IOException {
         //读入TXT文件
         Scanner input=new Scanner(System.in);
@@ -18,19 +17,19 @@ public class Main {
         List<String> lists = new ArrayList<String>();  //存储过滤后单词的列表
         String readLine = null;
         while((readLine = br.readLine()) != null){
-            //System.out.println(readLine);
             countChar += readLine.length();//字符个数就是字符长度
             counLine++;//一行一行读，每读一行行数加1
-            String[] wordsArr1 = readLine.split(" ");//以空格和非字母数字符号分割，至少4个英文字母开头，跟上字母数字符号
+            String[] wordsArr1 = readLine.split("\\s*[^0-9a-zA-Z]+");//以空格和非字母数字符号分割，至少4个英文字母开头，跟上字母数字符号
             for (String word : wordsArr1) {
+                if(word.matches("[a-zA-Z]{4,}[a-zA-Z0-9]*")){
                     lists.add(word);
+                }
             }
         }
         reader.close();//关闭文件
-////        wordcount(lists);
-    Map<String, Integer> wordsCount = new TreeMap<String,Integer>();  //存储单词计数信息，key值为单词，value为单词数
 
-    //单词的词频统计
+         Map<String, Integer> wordsCount = new TreeMap<String,Integer>();  //存储单词计数信息，key值为单词，value为单词数
+         //单词的词频统计
         for (String li : lists) {
         if(wordsCount.get(li) != null){
             wordsCount.put(li,wordsCount.get(li) + 1);
@@ -38,16 +37,16 @@ public class Main {
             wordsCount.put(li,1);
         }
             countWord++;
-    }
+        }
+
         System.out.println("characters:"+countChar);
         System.out.println("words:"+countWord);
         System.out.println("lines:"+counLine);
         SortMap(wordsCount);    //按值进行排序
-
 }
 
-    //按value的大小进行排序
-    public static void SortMap(Map<String,Integer> oldmap){
+        //按value的大小进行排序
+        public static void SortMap(Map<String,Integer> oldmap){
         ArrayList<Map.Entry<String,Integer>> list = new ArrayList<Map.Entry<String,Integer>>(oldmap.entrySet());
         Collections.sort(list,new Comparator<Map.Entry<String,Integer>>(){
             @Override
@@ -55,28 +54,20 @@ public class Main {
                 return o2.getValue() - o1.getValue();  //降序
             }
         });
-        for(int i = 0; i<10; i++){//list.size() 只输出频率最高的10个
-            System.out.println(list.get(i).getKey()+ ": " +list.get(i).getValue());
-        }
-    }
-
-
-            //写入Txt文件
-            /*File writename = new File(".\\test\\output.txt"); // 相对路径，如果没有则要建立一个新的output。txt文件
-            writename.createNewFile(); // 创建新文件
-            BufferedWriter out = new BufferedWriter(new FileWriter(writename));
-            out.write("我会写入文件啦\r\n"); // \r\n即为换行
-            out.flush(); // 把缓存区内容压入文件
-            out.close(); // 最后记得关闭文件*/
-
-
-    /*public static int wordcount(List<String> input){
-        for (String word : input) {
-            char[] ch=word.toCharArray();//ch每个单词
-            for(char c:ch) {//c是单词中每个字符
+        if(list.size()>=10){
+            for(int i = 0; i<10; i++){//list.size() 只输出频率最高的10个
+                System.out.println(list.get(i).getKey()+ ": " +list.get(i).getValue());
+            }
+        }else {
+            for(int i = 0; i<list.size(); i++){//list.size() 只输出频率最高的10个
+                System.out.println(list.get(i).getKey()+ ": " +list.get(i).getValue());
             }
         }
-        return  0;
-    }*/
+
+    }
+
+        //写入Txt文件
+//         File writename = new File("output.txt");
+
 }
 
