7729a37dd546e22acb78e9c1af260f639f4035b8
wing25 <38413060+wing25@users.noreply.github.com>
2018-07-09 15:26:30 +0800

    v2
    统计正确

--- src/WordCount.java
+++ src/WordCount.java
@@ -11,22 +11,25 @@ public class WordCount {
         try {
             FileInputStream in = new FileInputStream(file);
             int c;
-            int i = 0;
+            int i = 1;
             int w=0;
             int cc=0;
             while ((c = in.read()) > -1) {
-                w++;
-                if ((char) c == ' '||c =='\n') cc++; //统计单词数
+                if ( c != ' ' && c!='\n')
+                    cc++; //统计字符数
+                if ((char) c == ' '||c =='\n') w++; //统计单词数
                 if ((char) c == '\n') i++; //统计行数
 
                 System.out.print((char) c);
             }
             in.close();
             System.out.flush();
-            System.out.println("\n\n\n----------------");
+            System.out.println("----------------");
+            System.out.println("File " + args[0] + " chars: " +cc);
+            System.out.println("File " + args[0] + " words: " +w);
             System.out.println("File " + args[0] + " Lines: " + i);
-            System.out.println("File " + args[0] + " chars: " + w);
-            System.out.println("File " + args[0] + " words: " + cc);
+
+
         } catch (FileNotFoundException e) {
             System.err.println(file + " is not found");
         } catch (IOException e) {
