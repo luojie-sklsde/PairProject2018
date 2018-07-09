0a67a9770270bc8ee5d36072c88d79df217ad9d5
wing25 <38413060+wing25@users.noreply.github.com>
2018-07-09 16:24:13 +0800

    new1
    独立类定义

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String ifileName="", ofileName="";
+        int m = 10;
+
+        if (args.length < 1) {
+            System.err.println("Usage: java WordCount -n 2 -i <input_file> -o <output_file>");
+            System.exit(-1);
+        }
+
+        for(int i = 0; i < args.length; i++) {
+            if (args[i].compareTo("-i") == 0)
+                ifileName = args[i + 1];
+            if (args[i].compareTo("-o") == 0)
+                ofileName = args[i + 1];
+            if (args[i].compareTo("-m") == 0)
+                m = Integer.parseInt(args[i+1]);
+
+        }
+        File ifile = new File(ifileName);
+        File ofile = new File(ofileName);
+
+        WordCount mwc =  new WordCount();
+        mwc.setter(ifile, 5, ifile);
+        System.out.println("----------------");
+        System.out.println("File " + ifileName + " chars: " +  mwc.getCharacters());
+        System.out.println("File " + ifileName + " words: " +  mwc.getWords());
+        System.out.println("File " + ifileName + " Lines: " +  mwc.getLines());
+
+
+
+    }
+}
--- src/WordCount.java
+++ src/WordCount.java
@@ -2,36 +2,47 @@ import java.io.*;
 
 
 public class WordCount {
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Usage: java FileType <input_file>");
-            System.exit(-1);
-        }
-        File file = new File(args[0]);
+
+    private int lines;
+    private int words;
+    private int characters;
+
+    public int getLines() {
+        return lines;
+    }
+
+    public int getWords() {
+        return words;
+    }
+
+    public int getCharacters() {
+        return characters;
+    }
+
+
+    public  void setter(File ifile, int m, File ofile) {
+        int c = 0;
         try {
-            FileInputStream in = new FileInputStream(file);
-            int c;
-            int i = 1;
-            int w=0;
-            int cc=0;
+            FileInputStream in = new FileInputStream(ifile);
+
             while ((c = in.read()) > -1) {
-                if ( c != ' ' && c!='\n')
-                    cc++; //统计字符数
-                if ((char) c == ' '||c =='\n') w++; //统计单词数
-                if ((char) c == '\n') i++; //统计行数
+                characters++; //统计字符数
+                if ((char) c == ' ' || c == '\n') words++; //统计单词数
+                if ((char) c == '\n') lines++; //统计行数
 
                 System.out.print((char) c);
             }
             in.close();
             System.out.flush();
-            System.out.println("----------------");
-            System.out.println("File " + args[0] + " chars: " +cc);
-            System.out.println("File " + args[0] + " words: " +w);
-            System.out.println("File " + args[0] + " Lines: " + i);
+
+            System.out.println(characters);
+            System.out.println(words);
+            System.out.println(lines);
+
 
 
         } catch (FileNotFoundException e) {
-            System.err.println(file + " is not found");
+            System.err.println(ifile + " is not found");
         } catch (IOException e) {
             e.printStackTrace();
         }
