2bbea0ea2d7929708ab6ef8d988d5c1cc2b84fac
wing25 <38413060+wing25@users.noreply.github.com>
2018-07-09 18:02:53 +0800

    new2
    可以查询每个的单词的频率

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 
 
 public class WordCount {
@@ -6,6 +7,9 @@ public class WordCount {
     private int lines;
     private int words;
     private int characters;
+    private ArrayList<String> wordArr = new ArrayList<>();
+    private String  word = "";
+    private int[] count;
 
     public int getLines() {
         return lines;
@@ -26,8 +30,15 @@ public class WordCount {
             FileInputStream in = new FileInputStream(ifile);
 
             while ((c = in.read()) > -1) {
+                if( c != ' ' && c != '\n' && c != 0xd)
+                word += c;
+
                 characters++; //统计字符数
-                if ((char) c == ' ' || c == '\n') words++; //统计单词数
+                if ((char) c == ' ' || c == '\n') {
+                    words++; //统计单词数
+                    wordArr.add(word);
+                    word = "";
+                }
                 if ((char) c == '\n') lines++; //统计行数
 
                 System.out.print((char) c);
@@ -35,11 +46,29 @@ public class WordCount {
             in.close();
             System.out.flush();
 
+            /*
             System.out.println(characters);
             System.out.println(words);
             System.out.println(lines);
 
-
+            for(int i=0; i < wordArr.size(); i++)
+                System.out.println((String)wordArr.get(i));
+            */
+            count = new int[wordArr.size()];
+            for(int i=0; i < wordArr.size(); i++) {
+                String aEach = (String) wordArr.get(i);
+                count[i]=1;
+                for (int j = i + 1; j < wordArr.size(); j++){
+                    String aEach1 = (String) wordArr.get(j);
+                    if(aEach.equals(aEach1)){
+                        count[i]++;
+                    //System.out.println("cout["+i+"]"+count[i]);
+                    }
+                }
+            }
+            for(int i=0; i < wordArr.size(); i++){
+                
+            }
 
         } catch (FileNotFoundException e) {
             System.err.println(ifile + " is not found");
